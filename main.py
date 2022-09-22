3da4a6048a339c765e2844dc137d1cf255435fb1
Melanie Burkhart <16jlboyle@gmail.com>
2022-09-21 23:23:41 -0500

    completed project with nested loop

--- main.py
+++ main.py
@@ -16,84 +16,96 @@ transportation = ['train', 'bus', 'RV', 'Harley']
 # list of entertainment
 entertainment = ['concert', 'comedy act', 'boat ride', 'hike']
 
-# Create Dictionary
+#nested while loop to select choices from list and approve of trip or redo
+
+valid_answer = "no"
+
+while valid_answer == "no":
 
 # Determine destination with user input
 
-valid_response = "no"
+    valid_response = "no"
 
-while valid_response == "no":
+    while valid_response == "no":
 
-    destination = random.choice (destinations)
-    destination_input = input(f"Would you like to visit {destination} (yes or no)? ")
+        destination = random.choice (destinations)
+        destination_input = input(f"Would you like to visit {destination} (yes or no)? ")
         
-    if destination_input == "yes":
-        print(f"{destination} is a nice place to visit")
-        valid_response = "yes"
-    else:
-        print ("Here is another option")
+        if destination_input == "yes":
+            print(f"{destination} is a nice place to visit")
+            valid_response = "yes"
+        else:
+            print ("Here is another option")
 
 # Determine restaurant with user input
 
-valid_response = "no"
+    valid_response = "no"
 
-while valid_response == "no":
+    while valid_response == "no":
 
-    restaurant = random.choice (restaurants)
-    restaurant_input = input (f"Would you like to eat at {restaurant}  (yes or no)?  ")
+        restaurant = random.choice (restaurants)
+        restaurant_input = input (f"Would you like to eat at {restaurant}  (yes or no)?  ")
 
-    if restaurant_input == "yes":
-        print (f"{restaurant} is a great place to eat")
-        valid_response = "yes"
-    else:
-        print ("Here is another option")
+        if restaurant_input == "yes":
+            print (f"{restaurant} is a great place to eat")
+            valid_response = "yes"
+        else:
+            print ("Here is another option")
 
 # Determine transportation with user input
 
-valid_response = "no"
+    valid_response = "no"
 
-while valid_response == "no":
+    while valid_response == "no":
 
-    transport = random.choice (transportation)
-    transport_input = input (f"Would you like to get there by {transport}  (yes or no)?   ")
+        transport = random.choice (transportation)
+        transport_input = input (f"Would you like to get there by {transport}  (yes or no)?   ")
 
-    if transport_input == "yes":
-        print (f"{transport} is a fun way to get there")
-        valid_response = "yes"
-    else:
-        print ("Here is another option")
+        if transport_input == "yes":
+            print (f"{transport} is a fun way to get there")
+            valid_response = "yes"
+        else:
+            print ("Here is another option")
 
 # Determine entertainment with user input
 
-valid_response = "no"
+    valid_response = "no"   
 
-while valid_response == "no":
+    while valid_response == "no":
 
-    entertain = random.choice (entertainment)
-    entertain_input = input (f"Would you like to go to {entertain}  (yes or no)?  ")
+        entertain = random.choice (entertainment)
+        entertain_input = input (f"Would you like to go to {entertain}  (yes or no)?  ")
 
-    if entertain_input == "yes":
-        print (f"A {entertain} is a nice experience")
-        valid_response = "yes"
-    else:
-        print ("Here is another option")
+        if entertain_input == "yes":
+            print (f"A {entertain} is a nice experience")
+            valid_response = "yes"
+        else:
+            print ("Here is another option")
 
 # Display the initial random trip
 
-print (f"Visiting {destination}, eating at {restaurant}, traveling by {transport}, and {entertain} makes for a great daytrip" )
+    print (f"Your trip to {destination}, eating at {restaurant}, \ntaking the {transport}, and enjoying a {entertain} is planned")
 
 # User input on whether satisfied with trip
+# Will loop to provide other trip options if not satisfied with trips
+# Prints out trip selections
 
-# Change trip feature
+    day_trip_input = input (f"Would you like to go on this day trip (yes or no)?   ")
 
-# Display completed trip:
+    if day_trip_input == "yes":
+        print ("Have fun on this day trip")
+        valid_answer = "yes"
+    else:
+        print ("Lets try other selections for your day trip")
 
-# my_dictionary = {"destinations": (destination), "restaurants": (restaurant),"transportation": (transport), "entertainment": (entertain)}
+
+
+
+
 
 
     
-# print (f"Visiting {my_dictionary [destinations], [restaurants]}")
-# print (my_dictionary [destination], [restaurant], [transportation], [entertainment])
+
 
 
 
